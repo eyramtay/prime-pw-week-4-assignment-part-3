faffbaf5375f633015f1b0e742e3fa4c0edb5f8c
eyramtay <71682389+eyramtay@users.noreply.github.com>
2021-03-21 23:01:53 -0500

    Completed assignment

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,30 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+console.log('Current basket array should be showing as empty: ', basket);
+
+function addItem(item) {
+  let newBasket = basket.push(item);
+
+  return true;
+}
+// console.log(`Basket is now ${newBasket}`);
+console.log(addItem('Apples'), basket);
+console.log(addItem('Oranges'), basket);
+console.log(addItem('Bananas'), basket);
+
+
+function listItems() {
+  for (let i = 0; i < basket.length; i++) {
+
+ }
+}
+console.log(listItems());
+
+function empty() {
+  const emptyBasket = basket.slice(basket.length);
+  return emptyBasket;
+}
+console.log('Basket array should be empty: ', empty());
